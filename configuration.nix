c15e4f91fb3d6fd01faa3ef464803eae30c5fcfa
tamanaka.gustavo@gmail.com <tamanaka.gustavo@gmail.com>
2025-09-14 11:28:24 -0300

    Add support to netbird

--- configuration.nix
+++ configuration.nix
@@ -145,9 +145,13 @@
      distrobox
      distroshelf
      koji
+     netbird-ui
   ];
 
   programs.starship.enable = true;
+  services.netbird = {
+  	enable = true;
+  }
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
